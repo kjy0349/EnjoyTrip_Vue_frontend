d454d21a4814b5e6dc8d69945924774f2f78c6b2
KimYoungUk <dmnion@naver.com>
2023-11-10 16:11:21 +0900

    router

---
+++ src/components/board/BoardList.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1
+      class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+    >
+      게시판
+    </h1>
+  </div>
+</template>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import MainView from '@/views/MainView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import MainView from "@/views/MainView.vue";
+import BoardList from "@/components/BoardList.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: MainView
-    }
-  ]
-})
+      path: "/",
+      name: "home",
+      component: MainView,
+    },
+    {
+      path: "/board",
+      name: "BoardList",
+      component: BoardList,
+    },
+  ],
+});
 
-export default router
+export default router;
