99fcb300e861693a9eebb4a561746d884348e29f
kjy0349 <kjy0349@naver.com>
2023-11-10 15:27:58 +0900

    first

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,12 +1,18 @@
 <script setup>
-
+import { FwbButton } from 'flowbite-vue'
 </script>
 
 <template>
-	<div>
-		<h1>시작 페이지입니다.</h1>
-	</div>
-</template>
+	<fwb-button color="default">Default</fwb-button>
+	<fwb-button color="alternative">Alternative</fwb-button>
+	<fwb-button color="dark">Dark</fwb-button>
+	<fwb-button color="light">Light</fwb-button>
+	<fwb-button color="green">Green</fwb-button>
+	<fwb-button color="red">Red</fwb-button>
+	<fwb-button color="yellow">Yellow</fwb-button>
+	<fwb-button color="purple">Purple</fwb-button>
+	<fwb-button color="pink">Pink</fwb-button>
+  </template>
 
 <style scoped>
 
