981aaed56bdfaf98e0d4f5cf547158197e882a6e
KimYoungUk <dmnion@naver.com>
2023-11-13 01:31:43 +0900

    list, detail, write, modify, delete(미완성)

--- src/components/board/BoardWrite.vue
+++ src/components/board/BoardWrite.vue
@@ -1,7 +1,21 @@
-<script setup></script>
+<script setup>
+import BoardWriteItem from "./item/BoardWriteItem.vue";
+</script>
 
 <template>
-  <div></div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-10">
+        <h2 class="my-3 py-3 shadow-sm bg-light text-center">
+          <mark class="sky">글쓰기</mark>
+        </h2>
+      </div>
+      <div class="col-lg-10 text-start">
+        <!-- props로 글 작성인 걸 알려줄 write를 보냄 -->
+        <BoardWriteItem type="write" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
--- src/views/TheBoardView.vue
+++ src/views/TheBoardView.vue
@@ -1,9 +1,27 @@
-<script setup></script>
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const moveWrite = () => {
+  router.push({ name: "board-write" });
+};
+</script>
 
 <template>
-  <div class="text-center">
-    <h1 class="text-success my-3">게시판입니다</h1>
-    <router-view></router-view>
+  <div class="container">
+    <div class="text-center row">
+      <h1 class="text-success my-3">게시글</h1>
+      <p>여러분의 경험을 공유해주세요</p>
+      <div class="row justify-content-end">
+        <div class="col-2 my-3">
+          <button type="button" class="btn btn-secondary" @click="moveWrite">
+            글쓰기
+          </button>
+        </div>
+      </div>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
