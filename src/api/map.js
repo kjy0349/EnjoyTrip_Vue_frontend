568481175275455a3c05b7c4c4685e94ff16ff2d
kjy0349 <kjy0349@naver.com>
2023-11-14 15:24:18 +0900

    feat: map marker added, list added

---
+++ src/api/map.js
@@ -0,0 +1,29 @@
+import { localAxios } from '@/util/http-commons'
+const local = localAxios()
+
+function getCityOptions(selectId) {
+  local.get('info/sidoinfo').then(({ data }) => {
+    let sel = document.getElementById(selectId)
+    data.data.forEach((area) => {
+      let opt = document.createElement('option')
+      opt.setAttribute('value', area.sidoCode)
+      opt.appendChild(document.createTextNode(area.sidoName))
+      sel.appendChild(opt)
+    })
+  })
+}
+
+async function getAttractionList() {
+  let keyword = document.getElementById('search-keyword').value
+  if (keyword.length > 0) {
+    let sidoCode = document.getElementById('search-area-sido').value
+    let contentId = document.getElementById('search-content-id').value
+    let keyword = document.getElementById('search-keyword').value
+
+    return await local.get('info/attinfo/' + contentId + '/' + sidoCode + '/' + keyword)
+  } else {
+    alert('검색어를 입력해주세요.')
+  }
+}
+
+export { getCityOptions, getAttractionList }
