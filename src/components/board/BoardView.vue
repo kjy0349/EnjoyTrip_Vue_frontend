5ce9c288097122e3997dc9dda63b9b9eaf838fa1
KimYoungUk <dmnion@naver.com>
2023-11-16 00:15:26 +0900

    style : CommentStyle feat : ShowComments

--- src/components/board/BoardView.vue
+++ src/components/board/BoardView.vue
@@ -11,7 +11,37 @@ const router = useRouter();
 const { articleno } = route.params;
 
 const article = ref({});
-const comment = ref("");
+const comments = ref([
+  {
+    commentNo: 0,
+    userId: "",
+    articleNo: 0,
+    content: "",
+    registerTime: "",
+  },
+  {
+    commentNo: 1,
+    userId: "1",
+    articleNo: 1,
+    content: "댓글1",
+    registerTime: "1",
+  },
+  {
+    commentNo: 2,
+    userId: "2",
+    articleNo: 2,
+    content: "댓글2",
+    registerTime: "2",
+  },
+  {
+    commentNo: 3,
+    userId: "3",
+    articleNo: 3,
+    content: "댓글3",
+    registerTime: "3",
+  },
+]);
+const commentList = ref([]);
 
 onMounted(() => {
   getArticle();
@@ -45,6 +75,17 @@ function onDeleteArticle() {
     }
   );
   moveList();
+}
+
+function onCreateComment() {
+  console.log(comments.value.content);
+  alert(comments.value.content);
+  comments.value.articleNo = articleno;
+  // TODO: 세션에 있는 유저의 ID값을 가져와서 content.value.userId값에 넣을 것
+  // 그리고 writeComment함수를 통해 댓글을 저장할 것
+  // 그리고 이 페이지를 새로고침 할 것
+
+  router.go();
 }
 </script>
 
@@ -83,25 +124,34 @@ function onDeleteArticle() {
             {{ article.content }}
           </div>
           <div class="divider mt-3 mb-3"></div>
-          <div class="input-group mb-3">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Recipient's username"
-              aria-label="Recipient's username"
-              aria-describedby="button-addon2"
-            />
-            <button
-              class="btn btn-outline-secondary"
-              type="button"
-              id="button-addon2"
-            >
-              Button
-            </button>
+          <!-- 댓글 입력 -->
+          <div class="row d-flex justify-content-center">
+            <!-- <div class="col-md-8 col-lg-6"> -->
+            <div>
+              <div
+                class="card shadow-0 border"
+                style="background-color: #f0f2f5"
+              >
+                <div class="card-body p-4">
+                  <div class="form-outline mb-4">
+                    <input
+                      type="text"
+                      id="addANote"
+                      class="form-control"
+                      placeholder="댓글"
+                      v-model="comments.content"
+                      @keyup.enter="onCreateComment"
+                    />
+                  </div>
+                  <CommentItem
+                    v-for="comment in comments"
+                    :key="comment.commentNo"
+                    :comment="comment"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
-          <CommentItem></CommentItem><CommentItem></CommentItem
-          ><CommentItem></CommentItem>
-
           <div class="divider mt-3 mb-3"></div>
           <div class="d-flex justify-content-end">
             <button
--- src/components/board/comment/CommentItem.vue
+++ src/components/board/comment/CommentItem.vue
@@ -1,21 +1,31 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+  comment: Object,
+});
+</script>
 
 <template>
-  <div class="card mb-3">
-    <div class="row g-0">
-      <div class="col-md-4">
-        <img src="..." class="img-fluid rounded-start" alt="..." />
-      </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </p>
-          <p class="card-text">
-            <small class="text-body-secondary">Last updated 3 mins ago</small>
-          </p>
+  <div class="card mb-4">
+    <div class="card-body">
+      <p>{{ comment.content }}</p>
+
+      <div class="d-flex justify-content-between">
+        <div class="d-flex flex-row align-items-center">
+          <img
+            src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(4).webp"
+            alt="avatar"
+            width="25"
+            height="25"
+          />
+          <p class="small mb-0 ms-2">{{ comment.userId }}</p>
+        </div>
+        <div class="d-flex flex-row align-items-center">
+          <p class="small text-muted mb-0">추천수</p>
+          <i
+            class="far fa-thumbs-up mx-2 fa-xs text-black"
+            style="margin-top: -0.16rem"
+          ></i>
+          <p class="small text-muted mb-0">3</p>
         </div>
       </div>
     </div>
